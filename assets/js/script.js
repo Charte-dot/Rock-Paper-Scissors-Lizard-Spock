0af8b2e13d54a50ccef49d900aa59fcfeb71b0c4
Caroline Harte <caroline.harte@yahoo.com>
2022-03-19 14:20:36 +0000

    Add player choice recognition for button selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-const gameChoice = document.getElementById("game-choice")
-const playerChoice = document.getElementById("player-choice")
+const gameChoiceDisplay = document.getElementById("game-choice")
+const playerChoiceDisplay = document.getElementById("player-choice")
 const resultDisplay = document.getElementById("result")
 const startButton = document.getElementById("start")
 const restartButton = document.getElementById("restart")
@@ -8,6 +8,9 @@ const scoreArea = document.getElementById("score-area")
 const openingElement = document.getElementById("opening")
 const instructionElement = document.getElementById("instruction")
 const playButtons = document.getElementById("play-buttons")
+const possibleChoices = document.querySelectorAll("button")
+let playerChoice
+
 
 
 startButton.addEventListener('click', startGame)
@@ -20,11 +23,18 @@ function startGame() {
     rounddisplay.classList.remove('hide')
     scoreArea.classList.remove('hide')
     playButtons.classList.remove('hide')
-
-
-
+    gameChoiceDisplay.classList.remove('hide')
+    playerChoiceDisplay.classList.remove('hide')
+    resultDisplay.classList.remove('hide')
 
 }
+
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click' , (e) => {
+    playerChoice = e.target.id
+    playerChoiceDisplay.innerHTML = playerChoice
+  }))
+  
+
 
 
 
