2aab808d28062b896f3e27d4eb376e3453af66ac
Caroline Harte <caroline.harte@yahoo.com>
2022-03-20 19:31:20 +0000

    Add game restart funtion to restart game

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,15 +16,13 @@ let playerScore = 0;
 let gameScore = 0;
 
 
-/**
- *start button event listeners
- */
-
+//start button event listeners
+ 
 startButton.addEventListener('click', startGame);
 
-/**
- * start game
- */
+
+//start game
+ 
 function startGame() {
   startButton.classList.add("hide");
   restartButton.classList.add("hide");
@@ -40,21 +38,18 @@ function startGame() {
 
 }
 
-/**
- * random game move selection
- */
+//random game move selection
+ 
 function getGameChoice() {
-  const option = ['rock' , 'paper' , 'scissors' , 'lizard' , 'spock'];
+  const option = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
   const randomNumber = Math.floor(Math.random() * 5) + 1;
   return option[randomNumber];
- }
+}
 
-/**
- * game win, loose, draw senarios
- */
-
+//game win, loose, draw sequence
+ 
 function game(playerChoice) {
-const gameChoice = getGameChoice();
+  const gameChoice = getGameChoice();
   switch (playerChoice + gameChoice) {
     case 'rockscissors':
     case 'rocklizard':
@@ -66,7 +61,7 @@ const gameChoice = getGameChoice();
     case 'lizardpaper':
     case 'spockrock':
     case 'spockscissors':
-      win(playerChoice, gameChoice); 
+      win(playerChoice, gameChoice);
       break;
     case 'rockspock':
     case 'rockpaper':
@@ -78,70 +73,73 @@ const gameChoice = getGameChoice();
     case 'lizardscissors':
     case 'spockpaper':
     case 'spocklizard':
-      lose(playerChoice, gameChoice); 
+      lose(playerChoice, gameChoice);
       break;
     case 'rockrock':
     case 'paperpaper':
     case 'lizardlizard':
     case 'scissorscissors':
     case 'spockspock':
-      draw (playerChoice, gameChoice); 
+      draw(playerChoice, gameChoice);
       break;
-}
-  
+  }
+
 }
 
 //game button event listeners//
 
-  function main () {
-    rockOption.addEventListener('click', () => game("rock"));
-    
-    paperOption.addEventListener('click', () => game("paper"));
-    
-    scissorsOption.addEventListener('click', () => game("sissors"));
-    
-    lizardOption.addEventListener('click', () => game("lizard"));
-    
-    spockOption.addEventListener('click', () => game("spock"));
-  }
+function main() {
+  rockOption.addEventListener('click', () => game("rock"));
 
-  main();
+  paperOption.addEventListener('click', () => game("paper"));
 
-  //score tracker and result display//
-  function win(playerChoice, gameChoice) {
-    playerScore++;
-    playerScore_span.innerHTML = playerScore;
-    gameScore_span.innerHTML = gameScore;
-    resultDisplay.innerHTML = `${playerChoice} beats  ${gameChoice}.... You Win!` 
+  scissorsOption.addEventListener('click', () => game("sissors"));
 
-    if (userScore < 5){
-      
-  } else if(userScore === 5){
+  lizardOption.addEventListener('click', () => game("lizard"));
+
+  spockOption.addEventListener('click', () => game("spock"));
+}
+
+main();
+
+//score tracker and result display//
+
+function win(playerChoice, gameChoice) {
+  playerScore++;
+  playerScore_span.innerHTML = playerScore;
+  gameScore_span.innerHTML = gameScore;
+  resultDisplay.innerHTML = `${playerChoice} beats  ${gameChoice}.... You Win!`
+
+  if (playerScore > 4) {
     resultDisplay.innerHTML = "Game over, you win!"
     endGame();
   };
-  }
+}
 
-  function lose(playerChoice, gameChoice) {
-    gameScore++;
-    playerScore_span.innerHTML = playerScore;
-    gameScore_span.innerHTML = gameScore;
-    resultDisplay.innerHTML = `${playerChoice} loses to  ${gameChoice}.... You Loose!` 
+//lose function
 
-    if (usercore < 5){
-      
-  } else if(gameScore === 5){
+function lose(playerChoice, gameChoice) {
+  gameScore++;
+  playerScore_span.innerHTML = playerScore;
+  gameScore_span.innerHTML = gameScore;
+  resultDisplay.innerHTML = `${playerChoice} loses to  ${gameChoice}.... You Loose!`
+
+  if (gameScore > 4) {
     resultDisplay.innerHTML = "Game over, you Loose!"
     endGame();
   };
-  }
-
-  function draw(playerChoice, gameChoice){
-    resultDisplay.innerHTML = `${playerChoice} equal  ${gameChoice}.... Its a Draw!`
 }
 
+//draw function
+
+function draw(playerChoice, gameChoice) {
+  resultDisplay.innerHTML = `${playerChoice} equal  ${gameChoice}.... Its a Draw!`
+}
+
+//end game and reset scores to play again//
+
 function endGame() {
-  restartButton.classList.remove("hide")
+  restartButton.classList.remove("hide");
   rockOption.classList.add("hide");
   paperOption.classList.add("hide");
   scissorsOption.classList.add("hide");
@@ -149,12 +147,10 @@ function endGame() {
   spockOption.classList.add("hide");
   playerScore = 0;
   gameScore = 0;
-  playerScore_span.innerHTML = userScore;
-  gameScore_span.innerHTML = compScore;
+  playerScore_span.innerHTML = playerScore;
+  gameScore_span.innerHTML = gameScore;
 
 }
 
-restartButton.addEventListener('click' , startGame)
-
-
-  
+//event listener for restart button
+restartButton.addEventListener('click', startGame)
